e05e53a9f566ad1619714e84e2a109f5d95db43e
Robert Brook <robertbrook@fastmail.fm>
2019-07-01 12:07:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-console.log("hello eorld");
 
-print("it works");
+
+
+var shacljs = require('shacl-js');
+module['exports'] = function helloWorld (hook) {
+  // hook.req is a Node.js http.IncomingMessage
+  var host = hook.req.host;
+  // hook.res is a Node.js httpServer.ServerResponse
+  // Respond to the request with a simple string
+  hook.res.json(hook.params);
+};
