f469f2f50bb814508e743aa7e9d3f76d121c2137
Robert Brook <robertbrook@fastmail.fm>
2019-07-03 15:21:46 +0100

    Trying xml2js

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 
-var gexf = require('gexf');
+
+var parseString = require('xml2js').parseString;
 
 
 module['exports'] = function helloWorld (hook) {
   // hook.req is a Node.js http.IncomingMessage
   var host = hook.req.host;
+  
+  var xml = hook.params.xml;
+    parseString(xml, function (err, result) {
+      console.dir(result);
+    });
   // hook.res is a Node.js httpServer.ServerResponse
   // Respond to the request with a simple string
   hook.res.json(hook.params);
